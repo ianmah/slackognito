aea15e49da5c9146fc4236966d11c0170cf36e76
Ian <ianmmah@gmail.com>
2020-01-14 22:16:16 -0800

    abstract poll for element and remove message requests

--- content.js
+++ content.js
@@ -1,5 +1,16 @@
 console.log("Hello from your Messlackenger!")
 
+function pollForElement(elem, timeout, callback) {
+    const intervalPoll = setInterval(() => {
+        const element = document.querySelector(elem);
+        if (element){
+            clearInterval(intervalPoll);
+            callback();
+        }
+    }, 400);
+    setTimeout(() => clearInterval(intervalPoll), timeout);
+}
+
 const pollForConversationList = setInterval(() => {
     const convoList = document.querySelector('[aria-label="Conversation List"]');
     if (convoList){
@@ -9,11 +20,20 @@ const pollForConversationList = setInterval(() => {
 }, 200)
 
 function beginRestyle(convoList) {
-    const convoLink = convoList.children[1]
-    const convoLinkClasses = convoList.children[1].classList['value']
+    const convoLink = convoList.firstElementChild
+    const convoLinkClasses = convoList.firstElementChild.classList['value']
     
+    // Remove conversation images
     const convoImage = convoList.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild
     const convoImageClass = convoImage.classList['0']
     const convoImages = document.querySelectorAll('.'+convoImageClass)
-    console.log(convoImages)
+    convoImages.forEach(node => node.parentNode.removeChild(node))
+}
+
+pollForElement('#message-dots', 5000, removeMessageRequests)
+
+function removeMessageRequests() {
+    const conversations = document.querySelector('[aria-label="Conversations"]');
+    conversations.removeChild(conversations.firstElementChild)
+    console.log('removed')
 }
